50740ba40d851da8bb0eb2242b3fa56fc00ac1de
bea0266 <bea0266@naver.com>
2024-10-20 16:38:01 +0900

    css 수정,  메모 입력 후 일정 시간 지난 후 자동으로 저장시키는 기능 테스트 중

--- node/public/js/common.js
+++ node/public/js/common.js
@@ -11,3 +11,14 @@ async function changeCategory() {
         });
     });
 }
+
+function debounce(func, wait) {
+    let timeout;
+
+    return function(...args) {
+        const context = this;
+
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(context, args), wait);
+    };
+}
--- node/src/router/htmlRouter.js
+++ node/src/router/htmlRouter.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const { renderHTML } = require('../utils/common');
 const router = express.Router();
+const path = require('path');
 
+// 화면 파일 경로 기본값
+const DEFAULT_VIEW_DIR_PATH = '../views';
+
+
+/** 메인 화면 표시 */
 router.get('/', (req, res) => {
-	return renderHTML(res, '', 'index');
+	return res.sendFile(
+		path.join(__dirname, DEFAULT_VIEW_DIR_PATH, 'index.html')
+	);
 });
 
 module.exports = router;
