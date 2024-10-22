f0aef558195b4eee6bbb38e83adb4cf06e416636
bea0266 <bea0266@naver.com>
2024-10-22 20:22:04 +0900

    test commit

--- node/src/router/memoRouter.js
+++ node/src/router/memoRouter.js
@@ -26,6 +26,7 @@ router.get('/', (req, res) => {
 // 메모 등록
 router.post('/', async (req, res) => {
   const category = req.body.category;
+  console.log('revert test');
   return res.status(200).json({ category }); 
 });
 
