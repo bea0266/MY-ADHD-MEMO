9a4b4ca8e1a0bc85eb93d998e98b8c1a67e224ce
bea0266 <bea0266@naver.com>
2024-10-22 20:57:16 +0900

    Revert "Merge branch 'merge-test' into master"
    This reverts commit 4124b4a252908bc6ca80bf8ce9c316980a11438e, reversing
    changes made to edba85b1f12bb15a41d84d9232c3523f05c646f3.

--- node/src/router/memoRouter.js
+++ node/src/router/memoRouter.js
@@ -28,7 +28,6 @@ router.post('/', async (req, res) => {
   const category = req.body.category;
   console.log('revert test');
   console.log('이래도 안해줄꺼야?')
-  console.log('추가3')
   return res.status(200).json({ category }); 
 });
 
